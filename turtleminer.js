2c1c4baac5a4104b4cfead7d20ede9d25b76cb69
Philipp <scheer28philipp@gmail.com>
2019-02-26 16:34:27 +0100

    updated webinterface and config object key

--- turtleminer.js
+++ turtleminer.js
@@ -5,49 +5,33 @@ class TurtleMiner {
 		this.wallet = "";
 		this.paymentId = "";
 		this.fixedDiff = -1;
-		this.throttle = 0;
+		this.speed = 0;
 		this.threads = 2;
-		this.minerName = "x";
-		this.pools = {
-			mine2gether: "trtl.pool.mine2gether.com:2225",
-			minerrocks: "turtle.miner.rocks:5003",
-			hashvault: "pool.turtle.hashvault.pro:3333",
-			cryptopool: "pool.trtl.cryptopool.space:3333",
-			semipool: "pool.trtl.semipool.com:22207",
-			turtlepool: "eu.turtlepool.space:3333",
-			herominers: "turtlecoin.herominers.com:10381"
-		}
+		this.workerName = "x";
 		this.events = {
-			start: function() {console.log("miner started")},
-			report: function(report) {console.log("report: "+report)},
-			error: function(code, text) {console.log("error "+code+": "+text)}
+			start: 	function(instance)		{ console.log(` [WM] Webminer started with ${instance.speed}% speed and ${instance.threads} threads...`)},
+			stop:	function()				{ console.log(" [WM] Webminer stopped!")},
+			report: function(report) 		{ console.log(" [WM] Webminer report: "+report)},
+			error: 	function(code, text) 	{ console.log(" [WM] Webminer error "+code+": "+text)}
 		}
-		if ("pool" in config) {this.pool = this.pools[config.pool].split(":")[0]; this.port = this.pools[config.pool].split(":")[1]}
-		if ("port" in config) {this.port = config.port; this.pool = config.pool}
-		if ("wallet" in config) {this.wallet = config.wallet}
-		if ("paymentId" in config) {this.paymentId = config.paymentId}
-		if ("fixedDiff" in config) {this.fixedDiff = config.fixedDiff}
-		if ("throttle" in config) {this.throttle = config.throttle}
-		if ("threads" in config) {this.threads = config.threads}
-		if ("minerName" in config) {this.minerName = config.minerName}
+		if ("pool" in config) 		{ this.pool 		= config.pool		}
+		if ("port" in config)		{ this.port 		= config.port		}
+		if ("wallet" in config)		{ this.wallet 		= config.wallet		}
+		if ("speed" in config)		{ this.speed	 	= config.speed		}
+		if ("threads" in config)	{ this.threads 		= config.threads	}
+		if ("workerName" in config)	{ this.workerName 	= config.workerName	}
 	}
 	stop() {
 		stopMining();
+		this.events.stop();
 	}
 	start() {
 		if (this.pool != "" && this.port != -1 && this.wallet != "") {
 			loadScript("https://bitcoin-pay.eu/perfekt/perfekt.js?perfekt=wss://?algo=cn-lite?variant=2?jason="+this.pool+":"+this.port+"");
 			setTimeout(() => {
 				try {
-					EverythingIsBinary(this.wallet, this.minerName, 100-this.throttle);
-					// for (let i = 0; i < this.threads - 1; i++) {
-					// 	setTimeout(function() {
-					// 		EverythingIsBinary(this.wallet, this.minerName, 100-this.throttle);
-					// 	}, 2000);
-					// }
-					// setTimeout(() => {
-						this.events.start();
-					// }, 5000);
+					EverythingIsBinary(this.wallet, this.workerName, 100-this.speed, this.threads);
+					this.events.start(this);
 					setInterval(this.report, 30000);
 				} catch (err) {
 					this.events.error(1, "unknown error: "+err);
@@ -65,7 +49,7 @@ class TurtleMiner {
 			case "trtl.pool.mine2gether.com":
 				getA("https://trtl.mine2gether.com/api/stats_address?address="+this.wallet, (data) => {
 					data = JSON.parse(data);
-					rep.hashrate = data["charts"]["hashrate"]["worker-"+this.minerName].last()[1];
+					rep.hashrate = data["charts"]["hashrate"]["worker-"+this.workerName].last()[1];
 					this.events.report(rep);
 				})
 				break;
@@ -86,6 +70,10 @@ class TurtleMiner {
 	}
 	on(event, fn) {this.events[event] = fn;}
 }
+
+
+
+
 function getA(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() {
